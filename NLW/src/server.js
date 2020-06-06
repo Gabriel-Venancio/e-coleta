a37ac675f5a26fa6cba2d6c68914b0b47d3929f2
Gabriel Venancio <gabrielrvenancio.s@gmail.com>
2020-06-06 10:49:38 -0300

    refatorando código com o nunjucks

--- NLW/src/server.js
+++ NLW/src/server.js
@@ -4,12 +4,23 @@ const server = express()
 server.use(express.static("NLW/public"))
 
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("NLW/src/views", {
+    express: server,
+    noCache: true
+})
+
+
 server.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render(__dirname + "/views/index.html")
 })
 
 server.get("/create-point", (req, res) =>{
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render(__dirname + "/views/create-point.html")
+})
+
+server.get("/search", (req, res) =>{
+    return res.render(__dirname + "/views/search-results.html")
 })
 
 server.listen(3000)
