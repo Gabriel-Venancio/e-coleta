14861f6a596b1b2a091d0ca15701b64dcfdf544e
Gabriel Venancio <gabrielrvenancio.s@gmail.com>
2020-06-07 08:36:59 -0300

    pegando requisição do formulário e adcionando na página de pesquisa

--- NLW/src/server.js
+++ NLW/src/server.js
@@ -5,6 +5,8 @@ const db = require("./database/db")
 
 server.use(express.static("NLW/public"))
 
+server.use(express.urlencoded({ extend: true }))
+
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("NLW/src/views", {
@@ -17,13 +19,80 @@ server.get("/", (req, res) =>{
     return res.render(__dirname + "/views/index.html")
 })
 
+
+
 server.get("/create-point", (req, res) =>{
+
+    console.log(req.query)
+
     return res.render(__dirname + "/views/create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+    // console.log(req.body)
+
+
+    
+            const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (
+                ?,?,?,?,?,?,?
+            );
+        `
+        const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.item
+          ]
+
+        function afterInsertData(err) {
+            if(err) {
+                console.log(err)
+                return res.send("Erro no cadastro!")
+            }
+
+            console.log("Cadastrado com Sucesso")
+            console.log(this)
+
+            return res.render("/views/create-point.html", { saved: true })
+
+        }
+
+        db.run(query, values, afterInsertData)
+
+
+
+
+
+
+    
+})
+
+
+
 server.get("/search", (req, res) =>{
 
-    db.all(`SELECT * FROM places`, function(err, rows){
+    const search = req.query.search
+
+    if(search == "") {
+
+        return res.render(__dirname + "/views/search-results.html", { total: 0 })
+
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
                 if(err) {
                     return console.log(err)
                 } 
